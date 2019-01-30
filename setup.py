a70f9db0669fec796a76b160cb472afca6e43012
anton <anton@ml2grow.com>
2019-01-30 12:11:56 +0100

    removed 2.6 support

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(name='ascii-canvas',
       packages=find_packages(),
       classifiers=[
               'Programming Language :: Python',
-              'Programming Language :: Python :: 2.6',
               'Programming Language :: Python :: 2.7',
               'Programming Language :: Python :: 3.3',
               'Programming Language :: Python :: 3.4',

e41cf08d3315258f569738384f9289b6510bd6bb
Paul Schweizer <paulschweizer@gmx.net>
2019-01-30 11:17:12 +0100

    Adding missing info for pypi (#4)

--- setup.py
+++ setup.py
@@ -2,16 +2,21 @@ from setuptools import setup
 from setuptools import find_packages
 
 
+with open("README.md") as stream:
+    long_description = stream.read()
+
 setup(name='ascii-canvas',
-      version='1.2.0',
+      version='1.2.1',
       author='Paul Schweizer',
       author_email='paulschweizer@gmx.net',
       description='Treat strings like Items on a 2D Canvas.',
-      long_description='',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/PaulSchweizer/ascii-canvas',
       packages=find_packages(),
       classifiers=[
               'Programming Language :: Python',
+              'Programming Language :: Python :: 2.6',
               'Programming Language :: Python :: 2.7',
               'Programming Language :: Python :: 3.3',
               'Programming Language :: Python :: 3.4',

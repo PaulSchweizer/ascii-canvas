af09e3cbf6d3bbd416e78118b648fadf7db3b7c1
Paul Schweizer <paulschweizer@gmx.net>
2019-07-30 12:36:47 +0200

    Testing strip on import

--- ascii_canvas/__init__.py
+++ ascii_canvas/__init__.py
@@ -3,22 +3,27 @@ import os
 import sys
 
 
-if sys.version_info.major < 3 or sys.version_info.minor < 6:
+from strip_hints import strip_on_import
+strip_on_import(__file__, to_empty=False, no_ast=False, no_colon_move=False,
+                only_assigns_and_defs=False, py3_also=False)
 
-    from strip_hints import strip_hints_main, import_hooks
 
-    item_path = os.path.join(os.path.dirname(__file__), 'item.py')
-    canvas_path = os.path.join(os.path.dirname(__file__), 'canvas.py')
+# if sys.version_info.major < 3 or sys.version_info.minor < 6:
 
-    importer = import_hooks.StripHintsImporter()
-    stripper = strip_hints_main.HintStripper(
-        to_empty=False, no_ast=False,
-        no_colon_move=False, only_assigns_and_defs=False)
-    module_dir = os.path.dirname(os.path.realpath(item_path))
-    importer.stripper_fun_to_use[module_dir] = stripper.strip_type_hints_from_file
+#     from strip_hints import strip_hints_main, import_hooks
 
-    importer.module_info = (None, item_path, None)
-    item = importer.load_module(item_path)
+#     item_path = os.path.join(os.path.dirname(__file__), 'item.py')
+#     canvas_path = os.path.join(os.path.dirname(__file__), 'canvas.py')
 
-    importer.module_info = (None, canvas_path, None)
-    canvas = importer.load_module(canvas_path)
+#     importer = import_hooks.StripHintsImporter()
+#     stripper = strip_hints_main.HintStripper(
+#         to_empty=False, no_ast=False,
+#         no_colon_move=False, only_assigns_and_defs=False)
+#     module_dir = os.path.dirname(os.path.realpath(item_path))
+#     importer.stripper_fun_to_use[module_dir] = stripper.strip_type_hints_from_file
+
+#     importer.module_info = (None, item_path, None)
+#     item = importer.load_module(item_path)
+
+#     importer.module_info = (None, canvas_path, None)
+#     canvas = importer.load_module(canvas_path)

6a8596eecd2e37f2b011387463d76bdb62adae14
Paul Schweizer <paulschweizer@gmx.net>
2019-07-30 14:16:49 +0200

    Update __init__.py

--- ascii_canvas/__init__.py
+++ ascii_canvas/__init__.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 
-if sys.version_info.major < 3 and sys.version_info.minor < 6:
+if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 6):
     from strip_hints import strip_on_import
     strip_on_import(__file__, to_empty=False, no_ast=False, no_colon_move=False,
                     only_assigns_and_defs=False, py3_also=True)
